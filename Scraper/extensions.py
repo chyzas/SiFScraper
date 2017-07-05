3097939e0a672153fb11d859596144b189c14477
Mantas <chyzas@gmail.com>
2017-07-05 23:22:34 +0300

    from email to sqs

--- Scraper/extensions.py
+++ Scraper/extensions.py
@@ -7,45 +7,25 @@ from scrapy import signals
 from scrapy.mail import MailSender
 from Scraper.sif_models import *
 from Scraper.settings import WEBSITE
-from Scraper.settings import MAIL
-from collections import defaultdict
 import logging
+import boto.sqs
+from Scraper.settings import SQS
+from boto.sqs.message import RawMessage
+import json
 
 
 logging.basicConfig(filename='extensions.log',level=logging.DEBUG,)
-
-
-def send_mail(message, title, recipient):
-    try:
-        msg = MIMEMultipart()
-        msg['From'] = MAIL['from']
-        msg['To'] = recipient
-        msg['Subject'] = title
-
-        msg.attach(MIMEText(message, 'html', _charset='utf-8'))
-        mailServer = smtplib.SMTP(MAIL['server'], MAIL['port'])
-        mailServer.ehlo()
-        mailServer.starttls()
-        mailServer.ehlo()
-        mailServer.login(MAIL['user'], MAIL['pass'])
-        mailServer.sendmail(MAIL['from'], recipient, msg.as_string())
-        mailServer.close()
-    except Exception as e:
-        print e
-
 
 def group_items(data):
     results = {}
     for line in data:
         email = line['email']
-        filter = line['filter']
+        name = line['filter_name']
         if not results.has_key(email):
             results[email] = {}
-        if not results[email].has_key(filter):
-            results[email][filter] = {}
-            results[email][filter]['items'] = []
-            results[email][filter]['filter_name'] = line['filter_name']
-        results[email][filter]['items'].append(line)
+        if not results[email].has_key(name):
+            results[email][name] = []
+        results[email][name].append(line['id'])
     return results
 
 
@@ -74,7 +54,7 @@ class Mailer(object):
 
     def spider_closed(self, spider, reason):
         items = Results.\
-            select(Results, FosUser.email, Filter.filter_name).\
+            select(Results.id, FosUser.email, Filter.filter_name).\
             join(Filter).\
             join(FosUser).\
             join(Websites, on=Filter.site_id == Websites.id).\
@@ -82,45 +62,21 @@ class Mailer(object):
             where(Websites.name == spider.name).\
             dicts()
         grouped = group_items(items)
-        if grouped:
+        if grouped and self.num_items != len(items):
+            conn = boto.sqs.connect_to_region('eu-west-1',
+                                              aws_access_key_id=SQS['key'],
+                                              aws_secret_access_key=SQS['secret'])
+            queue = conn.get_queue('email')
             for email in grouped:
-                message = self.format_message(grouped[email])
-                try:
-                    send_mail(message, 'Nauji ' + spider.name + '.lt skelbimai', email)
-                except Exception as e:
-                    logging.error(e.message)
-                    print e.message
+
+                msg = RawMessage()
+                msg.set_body(json.dumps({
+                    'subject': 'Nauji ' + spider.name + ' skelbimai',
+                    'email': email,
+                    'template': 'results',
+                    'data': grouped[email]
+                }))
+                queue.write(msg)
 
         for i in items:
             Results.update(is_new=0).where(Results.id == i['id']).execute()
-
-
-    def format_message(self, filter_list):
-        msg = ''
-        try:
-            for filter in filter_list:
-                current = filter_list[filter]
-                msg += '<h3>' + current['filter_name'] + '</h3><br>'
-                msg += '<a href="' + WEBSITE + 'user/filter/' + str(filter) + '/delete'+'">Atsisakyti</a>'
-                msg += '<br>'
-                msg += '*' * 20 + '<br>'
-                for result in current['items']:
-                    msg += '<a href="' + result['url'] + '">' + result['url'] + '</a>' + '<br>'
-                    msg += 'Kaina: <strong>' + result['price'] + '</strong><br>'
-                    msg += result['title'] + '<br>'
-                    msg += result['details'] + '<br>'
-                    msg += '-' * 20 + '<br>'
-        except Exception as e:
-            print e.message
-        start = """\
-               <html>
-                 <head></head>
-                 <body>
-                 <br>
-            """
-        end = """
-       </body>
-     </html>
-     """
-
-        return start + msg + end
